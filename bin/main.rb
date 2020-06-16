dbb5784aeb6ae0940f1c408098e917e42edbdf7d
oloomoses <olooine@gmail.com>
2020-06-16 22:16:09 +0300

    corrected linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -4,70 +4,63 @@ require_relative '../lib/board.rb'
 
 puts 'Welcome to TIC TAC TOE'
 
-puts  'Firts player please enter your name'
-pl_1 = gets.chomp
+puts 'Firts player please enter your name'
+pl1 = gets.chomp
 
-puts "Okey #{pl_1} you have been assigned letter 'O'"
+puts "Okey #{pl1} you have been assigned letter 'O'"
 puts 'Second player please enter your name'
 
-pl_2 = gets.chomp
-puts "Perfect #{pl_2} you have been assigned letter 'X'"
+pl2 = gets.chomp
+puts "Perfect #{pl2} you have been assigned letter 'X'"
 
-team = Players.new([pl_1,'X'], [pl_2,'O'])
+team = Players.new([pl1, 'X'], [pl2, 'O'])
 
 play_ground = Board.new
 
-old_board  = play_ground.old_view
+old_board = play_ground.old_view
 
+while play_ground.all_moves < 10
+  puts "Okey #{play_ground.all_moves.even? ? team.first_player : team.second_player} please pick a number"
+  play = gets.chomp
 
-while play_ground.all_moves < 10 
-    puts "Okey #{play_ground.all_moves.even? ? team.first_player : team.second_player  } please pick a number"
-    play = gets.chomp
+  if play_ground.all_moves.even?
+    sign = team.second_player_sign
+    list_to_update = team.step_array_p1
+  else
+    sign = team.first_player_sign
+    list_to_update = team.step_array_p2
+  end
 
-    if play_ground.all_moves.even? 
-        sign = team.second_player_sign
-        list_to_update = team.step_array_p1 
-    else 
-        sign = team.first_player_sign 
-        list_to_update = team.step_array_p2
-    end 
-
-    if play_ground.valid_move?(play.to_i)
-        updated_board = play_ground.progress(play, sign)
-        team.player_steps(list_to_update, play)
-        play_ground.display(updated_board)
-    else
-       if play.to_i == 0 
-        puts "Only numbers are allowed (1-9), look at the board and select available spaces"
-       elsif play.to_i > 0 
-        puts "Number #{play} appears to be taken, please take an available number"
-       end
-       play_ground.display(old_board)
+  if play_ground.valid_move?(play.to_i)
+    updated_board = play_ground.progress(play, sign)
+    team.player_steps(list_to_update, play)
+    play_ground.display(updated_board)
+  else
+    if play.to_i.zero?
+      puts 'Only numbers are allowed (1-9), look at the board and select available spaces'
+    elsif play.to_i.positive?
+      puts "Number #{play} appears to be taken, please take an available number"
     end
-    puts "#{team.first_player}'s moves: #{team.step_array_p1}  and  #{team.second_player}'s moves:  #{team.step_array_p2} | Total plays #{play_ground.all_moves} Status #{team.winner}"
-    
- 
+    play_ground.display(old_board)
+  end
+  puts "#{team.first_player}'s moves:#{team.step_array_p1} and #{team.second_player}'s moves: #{team.step_array_p2}"
+  puts " | Total plays #{play_ground.all_moves} Status #{team.winner}"
+
   if team.game_status
-    
-    puts ""
+
+    puts ''
     puts "HEY #{team.winner.upcase} has won the game!"
-    puts ""
-    puts "***            ***"
-    puts "*** GAME OVEER ***"
-    puts "***            ***"
-    puts ""
-    puts ""
+    puts ''
+    puts '***            ***'
+    puts '*** GAME OVEER ***'
+    puts '***            ***'
+    puts ''
+    puts ''
     exit
-  elsif !team.game_status && play_ground.all_moves == 9 
-    puts "MY GOODNESS! this is a draw game #{play_ground.all_moves}" 
+  elsif !team.game_status && play_ground.all_moves == 9
+    puts "MY GOODNESS! this is a draw game #{play_ground.all_moves}"
     exit
-  end   
-   
+  end
+
   puts team.game_status
 end
-
-
- 
- 
-
-# rubocop:enable Style/GlobalVars
--- lib/board.rb
+++ lib/board.rb
@@ -1,23 +1,23 @@
 class Board
   attr_accessor :player, :sign, :position
 
-  @@board_data = [".",".",".",".",".",".",".",".","."]
+  @@board_data = ['.', '.', '.', '.', '.', '.', '.', '.', '.',]
   @@taken = []
   @@total_plays = 0
-  @@status  = 0
+  @@status = 0
 
   def display(board_data)
     y = 0
     puts "----------------"
     while y < board_data.length
-      if y == 2  || y == 5
+      if y == 2 || y == 5
         puts "  #{board_data[y]}  "
         puts "----------------"
-      elsif y == 4 || y == 1  || y == 7
+      elsif y == 4 || y == 1 || y == 7
         print " | #{board_data[y]} | "
       elsif y == 8
         print "  #{board_data[y]}  \n"
-      else        
+      else
         print "  #{board_data[y]}  "
       end
 
@@ -28,17 +28,17 @@ class Board
 
   def valid_move?(num)
     valid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    num.is_a?(Numeric) && @@taken.none?(num) && num > 0  && num < 10   ? true : false
+    num.is_a?(Numeric) && @@taken.none?(num) && num > 0 && num < 10 ? true : false
   end
 
   def progress(position, sign)
     @@total_plays += 1
-    
+
     @@board_data[(position.to_i - 1)] = sign
     @@taken << position.to_i
     @@board_data
   end
-  
+
   def old_view
     @@board_data
   end
